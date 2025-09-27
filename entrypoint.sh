b9034775cda450060f6e8cc50491a0e5dd83faf2
Chenzz00 <168355526+Chenzz00@users.noreply.github.com>
2025-09-27 19:42:04 +0800

    Delete entrypoint.sh

--- entrypoint.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-# Fallback to port 8000 if $PORT not set
-PORT=${PORT:-8000}
-
-# Start Gunicorn as pid1 (avoids catatonit errors)
-exec gunicorn PPMA.wsgi:application --bind 0.0.0.0:$PORT --workers 4
