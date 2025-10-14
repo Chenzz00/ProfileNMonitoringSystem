bc6a8e8c48b101402d2adfe408c597a7b1ec7913
Chenzz00 <168355526+Chenzz00@users.noreply.github.com>
2025-10-15 00:08:12 +0800

    Update settings.py

--- PPMA/settings.py
+++ PPMA/settings.py
@@ -34,6 +34,9 @@ cloudinary.config(
 # =======================
 # Firebase configuration
 # =======================
+
+FIREBASE_KEY_PATH = None
+
 def initialize_firebase():
     """Initialize Firebase using environment variable (Railway)."""
     if firebase_admin._apps:
@@ -283,6 +286,7 @@ SECURE_CONTENT_TYPE_NOSNIFF = True
 SECURE_HSTS_SECONDS = 31536000
 SECURE_HSTS_INCLUDE_SUBDOMAINS = True
 SECURE_HSTS_PRELOAD = True
+
 
 
 
